477937433c3a159130eec7c158d98b645d1a918f
Noch22 <noah.fakeadresse@gmail.com>
2024-03-28 01:55:52 +0100

    header maj

--- src/js/header.js
+++ src/js/header.js
@@ -20,11 +20,7 @@ const gradientBox = document.querySelector('.gradient');
 
  window.matchMedia("(max-width: 768px)").matches ? 
 
-tl.to(gradientBox, {
-    duration: 1, // Durée de l'animation en secondes
-    ease: 'power2.inOut', // Facilité d'animation (vous pouvez ajuster cela selon vos préférences)
-    yoyo: true // Faire l'animation en sens inverse à la fin
-}) : tl.to(gradientBox, {
+" " : tl.to(gradientBox, {
     duration: 10, // Durée de l'animation en secondes
     background: 'linear-gradient(135deg, #ffcc00, #ff6699)', // Nouveau dégradé
     ease: 'power2.inOut', // Facilité d'animation (vous pouvez ajuster cela selon vos préférences)
@@ -40,6 +36,11 @@ document.addEventListener('DOMContentLoaded', function() {
     gsap.set(".menu-item p", { y: 225 });
 
     const timeline = gsap.timeline({ paused: true });
+    const mobile = gsap.timeline({ paused: true });
+    if(window.matchMedia("(max-width: 768px)").matches){ 
+        gsap.set(".menu-item p", { clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)" });
+    }
+    
 
     timeline.to(".custom-logo-link", {
         duration: 0.5,
@@ -58,16 +59,8 @@ document.addEventListener('DOMContentLoaded', function() {
         duration: 1,
         ease: "power4.inOut",
     });
-
-    if(window.matchMedia("(max-width: 768px)").matches){ 
-    gsap.set(".menu-item p", { clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)" });
+    
     timeline.to(".menu-item p", {
-        duration: 1.5,
-        y: 0,
-        stagger: 0.2,
-        opacity: 1,
-        ease: "power4.out",
-    }, "-=1")} else {timeline.to(".menu-item p", {
         duration: 1.5,
         y: 0,
         clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
@@ -75,22 +68,61 @@ document.addEventListener('DOMContentLoaded', function() {
         ease: "power4.out",
         opacity: 1,
     }, "-=1");
-    }
+    
 
+    //MOBILE 
+
+    mobile.to(".custom-logo-link", {
+        duration: 0.5,
+        opacity: 0,
+        ease: "power4.inOut",
+    })
+
+    mobile.to(".menu_page", {
+        duration: 0.5,
+        y: 0,
+        opacity: 1,
+        ease: "power4.inOut",
+    }, "-=.5")
+
+    mobile.to(".header", {
+        duration: 1,
+        ease: "power4.inOut",
+    });
+
+    mobile.to(".menu-item p", {
+        duration: 1.5,
+        y: 0,
+        stagger: 0.2,
+        opacity: 1,
+        ease: "power4.out",
+    }, "-=1")
 
 if(togglebutton){
     togglebutton.addEventListener('click', function() {
         if (isOpen) {
+            if(window.matchMedia("(max-width: 768px)").matches){ 
+                mobile.reverse();
+            } else {
             timeline.reverse();
+            }
         } else {
+            if(window.matchMedia("(max-width: 768px)").matches){ 
+                mobile.play();
+            } else {
             timeline.play();
+            }
         }
         isOpen = !isOpen;
     });
 }
     links.forEach(link => {
     link.addEventListener('click', function() {
+        if(window.matchMedia("(max-width: 768px)").matches){ 
+            mobile.reverse();
+        } else {
         timeline.reverse();
+        }
         isOpen = !isOpen;
         togglebutton.classList.toggle('opened');
         menupage.classList.toggle('opened');
